6ef59f8f3752427910495c3f023e0d8e46a97d44
greg-chuchro <gchuchro@acm.org>
2021-09-28 23:12:15 +0900

    #1 GET request

---
+++ Calq.Server/CalqServer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace Calq.Server {
+    public class CalqServer {
+
+        internal class MessagingException : Exception { }
+
+        public string[] Prefixes { get; set; }
+
+        private readonly object root;
+
+        private readonly JsonSerializerOptions serializerOptions = new() {
+            IncludeFields = true
+        };
+
+        public CalqServer(object root) {
+            this.root = root;
+        }
+
+        public void Start() {
+
+            void ListenerCallback(IAsyncResult result) {
+                var listener = (HttpListener)result.AsyncState!;
+                var context = listener.EndGetContext(result);
+                var response = context.Response;
+
+                string responseBody;
+                try {
+                    responseBody = GetResponseString(context.Request);
+                } catch (MessagingException ex) {
+                    responseBody = ex.ToString();
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                } catch (Exception ex) {
+                    responseBody = ex.ToString();
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                }
+
+                var buffer = Encoding.UTF8.GetBytes(responseBody);
+                response.ContentLength64 = buffer.Length;
+                using var output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+            }
+
+            string GetResponseString(HttpListenerRequest request) {
+                if (request.Url == null) {
+                    throw new Exception("invalid url");
+                }
+
+                var obj = GetObjByPath(request.Url.AbsolutePath);
+
+                switch (request.HttpMethod) {
+                    case "GET":
+                        return JsonSerializer.Serialize(obj, serializerOptions);
+                    default:
+                        throw new Exception("unknown method");
+                }
+
+                throw new Exception();
+            }
+
+            object GetObjByPath(string path) {
+                var subPaths = path.Split('/');
+
+                object? obj = root;
+                foreach (var subPath in subPaths) {
+                    if (subPath == "") {
+                        continue;
+                    }
+                    if (obj == null) {
+                        throw new MessagingException();
+                    }
+
+                    if (obj is ICollection collection) {
+                        obj = Reflection.GetChildValue(collection, subPath);
+                    } else {
+                        obj = Reflection.GetFieldOrPropertyValue(obj, subPath);
+                    }
+                }
+
+                return obj;
+            }
+
+            var listener = new HttpListener();
+            foreach (var prefix in Prefixes) {
+                listener.Prefixes.Add(prefix);
+            }
+            listener.Start();
+
+            while (true) {
+                var result = listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+                result.AsyncWaitHandle.WaitOne();
+            }
+        }
+    }
+}
---
+++ Calq.Server/Reflection.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+
+namespace Calq.Server {
+    internal class Reflection {
+        public static object? GetFieldOrPropertyValue(object obj, string fieldOrPropertyName) {
+            var type = obj.GetType();
+            var field = type.GetField(fieldOrPropertyName);
+            if (field != null) {
+                return field.GetValue(obj);
+            } else {
+                var property = type.GetProperty(fieldOrPropertyName);
+                if (property != null) {
+                    return property.GetValue(obj);
+                }
+            }
+            throw new MissingMemberException();
+        }
+
+        public static object? GetOrInitializeFieldOrPropertyValue(Type type, object obj, string fieldOrPropertyName) {
+            object? value;
+            var field = type.GetField(fieldOrPropertyName);
+            if (field != null) {
+                value = field.GetValue(obj);
+                if (value == null) {
+                    value = Activator.CreateInstance(field.FieldType);
+                    field.SetValue(obj, value);
+                }
+                return value;
+            } else {
+                var property = type.GetProperty(fieldOrPropertyName);
+                if (property != null) {
+                    value = property.GetValue(obj);
+                    if (value == null) {
+                        value = Activator.CreateInstance(property.PropertyType);
+                        property.SetValue(obj, value);
+                    }
+                    return value;
+                }
+            }
+            throw new MissingMemberException();
+        }
+
+        public static object? InitializeFieldOrPropertyValue(Type type, object obj, string fieldOrPropertyName) {
+            object? value;
+            var field = type.GetField(fieldOrPropertyName);
+            if (field != null) {
+                value = Activator.CreateInstance(field.FieldType);
+                field.SetValue(obj, value);
+                return value;
+            } else {
+                var property = type.GetProperty(fieldOrPropertyName);
+                if (property != null) {
+                    value = Activator.CreateInstance(property.PropertyType);
+                    property.SetValue(obj, value);
+                    return value;
+                }
+            }
+            throw new MissingMemberException();
+        }
+
+        public static void SetFieldOrPropertyValue(Type type, object obj, string fieldOrPropertyName, object? value) {
+            var field = type.GetField(fieldOrPropertyName);
+            if (field != null) {
+                field.SetValue(obj, value);
+            } else {
+                var property = type.GetProperty(fieldOrPropertyName);
+                if (property != null) {
+                    property.SetValue(obj, value);
+                } else {
+                    throw new MissingMemberException();
+                }
+            }
+        }
+
+        private static object ParseValue(Type type, string value) {
+            object objValue;
+            try {
+                objValue = Type.GetTypeCode(type) switch {
+                    TypeCode.Boolean => bool.Parse(value),
+                    TypeCode.Byte => byte.Parse(value),
+                    TypeCode.SByte => sbyte.Parse(value),
+                    TypeCode.Char => char.Parse(value),
+                    TypeCode.Decimal => decimal.Parse(value),
+                    TypeCode.Double => double.Parse(value),
+                    TypeCode.Single => float.Parse(value),
+                    TypeCode.Int32 => int.Parse(value),
+                    TypeCode.UInt32 => uint.Parse(value),
+                    TypeCode.Int64 => long.Parse(value),
+                    TypeCode.UInt64 => ulong.Parse(value),
+                    TypeCode.Int16 => short.Parse(value),
+                    TypeCode.UInt16 => ushort.Parse(value),
+                    TypeCode.String => value,
+                    _ => throw new ArgumentException($"type cannot be parsed: {type.Name}"),
+                };
+            } catch (OverflowException ex) {
+                long min;
+                ulong max;
+                switch (Type.GetTypeCode(type)) {
+                    case TypeCode.Byte:
+                        min = byte.MinValue;
+                        max = byte.MaxValue;
+                        break;
+                    case TypeCode.SByte:
+                        min = sbyte.MinValue;
+                        max = (ulong)sbyte.MaxValue;
+                        break;
+                    case TypeCode.Char:
+                        min = char.MinValue;
+                        max = char.MaxValue;
+                        break;
+                    case TypeCode.Int32:
+                        min = int.MinValue;
+                        max = int.MaxValue;
+                        break;
+                    case TypeCode.UInt32:
+                        min = uint.MinValue;
+                        max = uint.MaxValue;
+                        break;
+                    case TypeCode.Int64:
+                        min = long.MinValue;
+                        max = long.MaxValue;
+                        break;
+                    case TypeCode.UInt64:
+                        min = (long)ulong.MinValue;
+                        max = ulong.MaxValue;
+                        break;
+                    case TypeCode.Int16:
+                        min = short.MinValue;
+                        max = (ulong)short.MaxValue;
+                        break;
+                    case TypeCode.UInt16:
+                        min = ushort.MinValue;
+                        max = ushort.MaxValue;
+                        break;
+                    default:
+                        throw;
+                }
+                throw new OverflowException($"{value} ({min}-{max})", ex);
+            } catch (FormatException ex) {
+                throw new FormatException($"value type mismatch: {value} is not {type.Name}", ex);
+            }
+            return objValue;
+        }
+
+        public static object? GetChildValue(ICollection collection, string key) {
+            return collection switch {
+                Array array => array.GetValue(int.Parse(key)),
+                IList list => list[int.Parse(key)],
+                IDictionary dictionary => dictionary[ParseValue(dictionary.GetType().GetGenericArguments()[0], key)],
+                _ => throw new Exception("unknown collection")
+            };
+        }
+    }
+}
