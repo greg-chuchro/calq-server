6f4eea4fc359eaa1f7a7430fd6eb99dfb34c080b
greg-chuchro <gchuchro@acm.org>
2021-11-09 23:23:44 +0900

    #9 unique namespace (#10)

--- Calq.Server/CalqServer.cs
+++ Calq.Server/CalqServer.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Text;
 using System.Text.Json;
 
-namespace Calq.Server {
+namespace Ghbvft6.Calq.Server {
     public class CalqServer {
 
         internal class MessagingException : Exception { }
